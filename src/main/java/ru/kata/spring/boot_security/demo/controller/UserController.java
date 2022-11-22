b6bd7faf235eb69a6d7169e9224f6b99c0b0b9a9
Cilez <kobrelislegryi@gmail.com>
2022-11-22 17:20:25 +0300

    Настроил проект

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,61 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "show";
+    }
+
+    @GetMapping("/new")
+    public String createNewUser(Model model){
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping()
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(Model model, @PathVariable("id") Long id){
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id){
+        userService.updateUser(id, user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.dao.UserDAO;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO){
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    @Transactional
+    @Override
+    public void saveUser(User user) {
+        userDAO.saveUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(Long id) {
+        userDAO.deleteUser(id);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDAO.getUserById(id);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(Long id, User updatedUser) {
+        userDAO.updateUser(id, updatedUser);
+    }
+}
